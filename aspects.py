bb803c8e635dbc25010b0b8218042e661b7e922a
Steven Wallace <snwallace@gmail.com>
2011-09-30 09:26:02 -0500

    Made the instance and class interfaces more standardized. Added wrapper to allow more robust use of the library.

--- aspects.py
+++ aspects.py
@@ -1,15 +1,40 @@
-def replace_function(old, new):
+import functools
+def wrapper(func):
+    def inner(*args):
+        return inner.wrapped(*args)
+    inner.wrapped = func
+    return inner
+
+def instance_wrapper(func, self):
+    def inner(*args):
+        return inner.wrapped(self, *args)
+    inner.wrapped = func
+    return inner
+
+def replace_function(old, advice):
     name = old.__name__
-    new.__name__ = name
+    new = advice(old)
+    functools.update_wrapper(new, old)
     if hasattr(old, 'im_class'): #this is from a class
         new.im_class = old.im_class
-        setattr(old.im_class, name, new)
-    elif hasattr(old, 'func_globals'):
-        old.func_globals[name] = new
+        new.im_self = old.im_self
+        if old.im_self: #this is from an instance of the class
+            #redefine new here, so that the last element doesn't
+            #have an implicit self
+            new = advice(old.im_func)
+            functools.update_wrapper(new, old)
+            setattr(old.im_self, name, instance_wrapper(new, old.im_self))
+        else:
+            setattr(old.im_class, name, wrapper(new))
     else:
         m = __import__(old.__module__)
-        setattr(m, name, new)
+        setattr(m, name, wrapper(new))
 
 def wrap_function(func, advice):
-    new = advice(func)
-    replace_function(func, new)
+    if hasattr(func, 'wrapped'):
+        new = advice(func.wrapped)
+        functools.update_wrapper(new, func.wrapped)
+        func.wrapped = new
+        return True
+
+    replace_function(func, advice)
--- test.py
+++ test.py
@@ -38,10 +38,48 @@ def test_class():
 
     return True
 
+def test_instance():
+    class C:
+        def t(self):
+            return 1
+
+    def inc(func):
+        def inner(self):
+            return func(self) + 1
+        return inner
+
+    c = C()
+    if c.t() != 1:
+        return False
+    aspects.wrap_function(c.t, inc)
+    if c.t() != 2:
+        return False
+    c = C()
+    if c.t() != 1:
+        return False
+    return True
+
+def test_nested():
+    @aspects.wrapper
+    def t():
+        return 1
+    def inc(func):
+        def inner():
+            return func() + 1
+        return inner
+    for i in range(1, 10):
+        if t() != i:
+            return False
+        aspects.wrap_function(t, inc)
+    return True
+
+
 def test():
     tests = [
             test_global,
             test_class,
+            test_instance,
+            test_nested,
             ]
     successes = 0
     for test in tests:
